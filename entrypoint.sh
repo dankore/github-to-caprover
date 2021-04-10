ea8673b2f7b145368cf0a1dd9708d003a8c0428a
Adamu M. Dankore <zimmazone@yahoo.com>
2021-04-10 10:47:25 -0500

    General cleaning

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+if [[ -z ${INPUT_SERVER} ]]; then
+  echo "Please add your Caprover URL."
+  exit 1
+fi
+if [[ [[ -z ${INPUT_PASSWORD} ]]; then
+  echo "Please add your Caprover password."
+  exit 1
+fi
+if [[ -z ${INPUT_APPNAME} ]]; then
+  echo "Please add your Caprover application name."
+  exit 1
+fi
+
 if [ x$INPUT_IMAGE != x ]
 then
   caprover deploy -h $INPUT_SERVER -p $INPUT_PASSWORD -a $INPUT_APPNAME -i $INPUT_IMAGE
